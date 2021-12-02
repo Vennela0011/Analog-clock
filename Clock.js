42eb8acf699a7fd42cb03411884f5fdba39336d2
Vennela0011 <72298768+Vennela0011@users.noreply.github.com>
2021-12-02 23:07:35 +0530

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -1,49 +1,41 @@
-
-
 //  Get access to the hands of our clock 
+
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
- 
 
-console.log("Number.MAX_VALUE: ", Number.MAX_VALUE, " Number.MIN_VALUE: ", Number.MIN_VALUE);
-console.log("Number.MAX_SAFE_INTEGER: ", Number.MAX_SAFE_INTEGER);
- 
+
 // Get the current date and time 
 var date = new Date();
-
 console.log(date);
- 
-// Assign the initial time to a set of variables 
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-let ms = date.getMilliseconds();
- 
-console.log("time: " + hr + ":" + min + ":" + sec + "." + ms);
- 
-// Some basic arithmetic to get th eposition of the hands
-let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
-let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
-let secPosition = sec * 360 / 60;
- 
-// Function to run the clock
-function runTheClock() {
- 
-  // Increment position of hands
-  secPosition += 6;
-  minPosition += (6 / 60);
-  hrPosition += (3 / 360);
- 
-  if (secPosition % 360 == 0) {
-    console.log("milliseconds: ", new Date().getMilliseconds());
-  }
 
-  // Update hand position on clock
-  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-}
+// Assign the initial time to a set of variables 
+let thehr = date.getHours();
+let themin = date.getMinutes();
+let thesec = date.getSeconds();
+console.log("Hour: " + thehr + " Minute: " + themin + " Second: " + thesec);
+
+
+// Some basic arithmetic to get the position of the hands
+let thehrPosition = thehr * 360 / 12 + themin * (360 / 60) / 12;
+let theminPosition = themin * 360 / 60 + thesec * (360 / 60) / 60;
+let thesecPosition = thesec * 360 / 60;
+
+// Function to run the clock
+function animateTheClock() {
  
+ // Increment position of hands
+  thehrPosition = thehrPosition + 3 / 360;
+  theminPosition = theminPosition + 6 / 60;
+  thesecPosition = thesecPosition + 6;
+
+   // Update hand position on clock
+  THEHOURHAND.style.transform = "rotate(" + thehrPosition + "deg)";
+  THEMINUTEHAND.style.transform = "rotate(" + theminPosition + "deg)";
+  THESECONDHAND.style.transform = "rotate(" + thesecPosition + "deg)";
+}
+
 // Run the clock every 1000 milliseconds
-var interval = setInterval(runTheClock, 1000);
+var interval = setInterval(animateTheClock, 1000);
+
+
